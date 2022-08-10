f2b1dde2085392e3a198661cda6a52984c21a72a
Frida de Sigley <fdesigley@gmail.com>
2022-08-10 08:43:20 +0100

    formatted code

--- download.py
+++ download.py
@@ -1,30 +1,31 @@
-import zipfile 
+import zipfile
 from pathlib import Path
 
 import sys
-from time import sleep 
+from time import sleep
 from sys import stdout
 from urllib import response
-import requests 
+import requests
 import gdown
-import tqdm 
+from tqdm import tqdm
 
-class get_dataset: 
 
-    def __init__(self,fid:str, url:str):
+class get_dataset:
+
+    def __init__(self, fid: str, url: str):
         self.fid = fid
         self.url = url
 
-    def parse_google_url(self,fid=None):
-        with open(fid,'r') as handle:
+    def parse_google_url(self, fid=None):
+        with open(fid, 'r') as handle:
             fid_list = handle.readlines()
-        self.fid_list =  fid_list
+        self.fid_list = fid_list
 
     def download_from_txt(self):
         parsed = self.parse_google_url(self.fid)
         data = sys.stdin.readlines()
         outputs = [f'batch_{b_num}.zip' for b_num in range(len(data))]
-        for line,output in zip(data,outputs):
+        for line, output in zip(data, outputs):
             line = line.strip(',')
             line = line.split('/')
             id = line[5]
@@ -33,24 +34,17 @@ class get_dataset:
 
     def get_zip(self):
         '''gets zipfile of data from url'''
-        resp = requests.get(self.url,stream=True)
+        resp = requests.get(self.url, stream=True)
         file = self.url.split('/')[-1]
         with open(file, 'wb') as hndl:
             for data in tqdm(resp.iter_content()):
                 hndl.write(data)
 
-    def unzip(self,out_dir:str):
+    def unzip(self, out_dir: str):
         '''checks if images dir exists
         makes if not, then infaltes/decompreses to directory'''
         out, fid = Path(out_dir), Path(self.fid)
         if not out.is_dir():
             out.mkdir()
-        with zipfile.ZipExtFile(out,'r') as hndl:
+        with zipfile.ZipExtFile(out, 'r') as hndl:
             hndl.extractall(out)
-
-    
-
-
-
-
- 
