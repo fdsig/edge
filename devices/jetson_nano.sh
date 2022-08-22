05e6e1f60cd51be0f0287204f4646f5cc98b01fc
Frida de Sigley <fdesigley@gmail.com>
2022-08-22 10:57:25 +0100

    adding setup bash and torch with cuda docs

---
+++ devices/jetson_nano.sh
@@ -0,0 +1,7 @@
+sudo apt-get -y update; 
+sudo apt-get -y install autoconf bc build-essential g++-8 gcc-8 clang-8 lld-8 gettext-base gfortran-8 iputils-ping libbz2-dev libc++-dev libcgal-dev libffi-dev libfreetype6-dev libhdf5-dev libjpeg-dev liblzma-dev libncurses5-dev libncursesw5-dev libpng-dev libreadline-dev libssl-dev libsqlite3-dev libxml2-dev libxslt-dev locales moreutils openssl python-openssl rsync scons python3-pip libopenblas-dev;
+sudo apt-get install libopenblas-base libopenmpi-dev
+wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+sudo apt-get install python3-pip libopenblas-base libopenmpi-dev libomp-dev
+pip3 install Cython
+pip3 install numpy torch-1.10.0-cp36-cp36m-linux_aarch64.whl
